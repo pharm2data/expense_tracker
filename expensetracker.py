52d21e77e9a8a2b171c5d6a90057cb155022d5cf
pharm2data <mollystephens321@gmail.com>
2024-02-04 20:33:44 -0500

    Expense Structure
    The structure of the expense tracker.

---
+++ expensetracker.py
@@ -0,0 +1,17 @@
+def main ():
+    print(f"Running the Expense Tracker!")
+    user_expense_amount()
+    saving_users_expense()
+    summarizing_users_expense()
+
+def user_expense_amount():
+    print("Obtaining the user's expense")
+
+def saving_users_expense():
+    print("Saving the user's expense!")
+
+def summarizing_users_expense():
+    print("Summarizing the users expense!")
+
+if __name__ == "__main__":
+    main()
