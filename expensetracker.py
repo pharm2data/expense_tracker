0069cc40924e71c6ecc460d461253b43e17a9997
pharm2data <mollystephens321@gmail.com>
2024-02-09 22:54:51 -0500

    Update expensetracker.py
    Added budget tracking information.

--- expensetracker.py
+++ expensetracker.py
@@ -1,9 +1,13 @@
 from expense import Expense
+import calendar
+import datetime
 
 def main ():
     print(f"Running the Expense Tracker!")
     expense_file_pathway = "expenses.csv"
     expense = user_expense_amount()
+    budget = 3000
+
     saving_users_expense(expense, expense_file_pathway)
     summarizing_users_expense(expense_file_pathway)
 
@@ -36,7 +40,7 @@ def saving_users_expense(expense, expense_file_pathway):
     with open(expense_file_pathway, "a") as f:
         f.write(f"{expense.name},{expense.amount},{expense.category}\n")
 
-def summarizing_users_expense(expense_file_pathway):
+def summarizing_users_expense(expense_file_pathway, budget):
     print("Summarizing the users expense!")
     expenses: list[Expense] = []
     with open( expense_file_pathway , "r") as f:
@@ -47,7 +51,7 @@ def summarizing_users_expense(expense_file_pathway):
             line_expense = Expense(name=expense_name, amount=float(expense_amount), category=expense_category)
             expenses.append(line_expense)
 
-    category_amount = []
+    category_amount = {}
     for expense in expenses:
         key = expense.category
         if key in category_amount:
@@ -55,6 +59,19 @@ def summarizing_users_expense(expense_file_pathway):
         else: 
             category_amount[key] = expense.amount
     print(category_amount)
+
+    total_spent = sum([ex.amount for ex in expenses])
+    print("The total that has been spent is ${total_spent:.2f} this month!")
+
+    budget_remaining = budget - total_spent
+    print("The total budget remaining is ${total_spent:.2f} this month!")
+
+    now = datetime.datetime.now()
+
+    days_in_month = calendar.monthrange(now.year, now.month)[1]
+    remaining_days = days_in_month - now.day    
+    daily_budget = budget_remaining / remaining_days
+    print("Budget per day: $ {daily_budget:.2f}")
 
 if __name__ == "__main__":
     main()
